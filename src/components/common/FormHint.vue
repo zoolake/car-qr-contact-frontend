87b2123475e6ded0e460f08428f510ad9a9a4e1b
zoolake <junhomoon224@gmail.com>
2026-01-07 17:55:00 +0900

    refactor : css 분리

--- src/components/common/FormHint.vue
+++ src/components/common/FormHint.vue
@@ -7,10 +7,3 @@
 <script setup>
 // 별도 로직 없음 (순수 UI 컴포넌트)
 </script>
-
-<style scoped>
-.form-hint {
-  font-size: 12px;
-  color: #9aa0a6; /* 옅은 회색 */
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './styles/index.css'
 
 createApp(App).use(router).mount('#app')
